4bd193267b63d6d235556b8c13b6e007dc8113fa
Vasmayer <lealimarco90@gmail.com>
2022-02-07 18:24:56 +0100

    es completed

---
+++ src/components/jumbo.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="jumbo">
+    <img src="../assets/spiaggia.jpeg" alt="spiaggia" class="center">
+</div>    
+</template>
+
+<script>
+
+export default{
+
+    name:'Jumbo',  
+}
+</script>
+<style scoped>
+.center
+{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/title.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="container">
+   <h1>La mia prima app con Vue CLI</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#container
+{
+  text-align: center;
+  color: brown;
+}
+</style>
